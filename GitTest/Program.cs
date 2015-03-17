0d71c054c0aed814c7b3637cd2301b96993eb3df
TotalONE <totalone69@gmail.com>
2015-03-16 21:56:41 -0400

    first repo

---
+++ GitTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdfasdfasd");
+            Console.WriteLine("bug 0 Fixed ");
+            Console.WriteLine("bug 0 Fixed ");  
+            Console.WriteLine("What if computers spoke english, and humans speaked in binary ");
+            //something 
+            Console.ReadLine();           
+            
+        }
+    }
+}
